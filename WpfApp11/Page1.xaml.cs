c2e45af201cfb2860b673b54dc650ec282cc5f01
Friliat <rz389905@gmail.com>
2024-02-14 06:42:05 +0300

    Добавьте файлы проекта.

---
+++ WpfApp11/Page1.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+         public class Dat
+        {
+            public double X;
+            public void GetInfo()
+            {
+                if (X > 0)
+                {
+                    X -= 8;
+                }
+                else if (X < 0)
+                {
+                    X += 6;
+                }
+                else
+                {
+                    X = 10;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Dat dat = new Dat();
+            dat.X = double.Parse(A.Text);
+            dat.GetInfo();
+            B.Text = $"{dat.X}";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Dat dat = new Dat();
+            dat.X = double.Parse(A.Text);
+            dat.GetInfo();
+            NavigationService.Navigate(new Page2(dat));
+        }
+    }
+}
---
+++ WpfApp11/Page2.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+using static WpfApp11.Page1;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2(Dat dat)
+        {
+            InitializeComponent();
+            Dat dat1;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page1());
+        }
+
+        public void GetInfo(double a , double b , double c)
+        {
+            double M = (a + Pow(b, 2)) / c;
+            double k = (int)M % 3;
+            double y = 0;
+            switch (M)
+            {
+                case 0: y = Log(a / b); break;
+                case 1: y = Exp(M + c); break;
+                default: y = Sqrt(Abs(Pow(a + b, 2) + c)); break;
+            }
+            Otv.Text = $"{y}";
+            Dat dat1 = new Dat();
+            double v = dat1.X + y;
+            symm.Text = $"{v}";
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(A.Text);
+            double b = double.Parse(B.Text);
+            double c = double.Parse(C.Text);
+            GetInfo(a, b, c);
+        }
+    }
+}
